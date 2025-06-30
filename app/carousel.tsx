9906ad77b6bc9bff667219d4593f80539ac4f779
raghavvram <raghavvram.johnson@gmail.com>
2025-06-30 20:35:36 +0530

    fixed syntax errors

--- app/carousel.tsx
+++ app/carousel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Carousel = () => {
+const Carousel: React.FC = () => {
   return (
     <div>
       <div className="carousel w-full">
@@ -8,6 +8,7 @@ const Carousel = () => {
           <img
             src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
             className="w-full"
+            alt="Slide 1"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
@@ -20,8 +21,9 @@ const Carousel = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img
-            src="https.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
             className="w-full"
+            alt="Slide 2"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">
@@ -34,8 +36,9 @@ const Carousel = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img
-            src="https.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
             className="w-full"
+            alt="Slide 3"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">
@@ -48,8 +51,9 @@ const Carousel = () => {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <img
-            src="https.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
             className="w-full"
+            alt="Slide 4"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">
